bfa6870849a63d89138549891bb220399680c149
sejall <33982474+sejall@users.noreply.github.com>
2018-09-16 06:32:25 +0530

    Add files via upload

---
+++ ncR and nPr.py
@@ -0,0 +1,15 @@
+# Python Program - Find ncR and nPr
+
+import math;
+print("Enter 'x' for exit.");
+nval = input("Enter value of n: ");
+if nval == 'x':
+    exit();
+else:
+    rval = input("Enter value of r: ");
+    n = int(nval);
+    r = int(rval);
+    npr = math.factorial(n)/math.factorial(n-r);
+    ncr = npr/math.factorial(r);
+    print("ncR =",ncr);
+    print("nPr =",npr);
